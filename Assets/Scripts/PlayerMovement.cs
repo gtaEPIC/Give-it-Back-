0e84e29b3f5a71411f4642ba8fb7bc6866acb62c
gtaEPIC <johnnygotrain@gmail.com>
2021-11-11 11:32:44 -0500

    Added animations to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public float speed, jumpPower;
+    public AnimatorController idle, run, jump, fall;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
     private int _jumpTimeout;
     private bool _canJump;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,7 +36,12 @@ public class PlayerMovement : MonoBehaviour
         else _jumpTimeout = 30;
         Vector2 newVelocity = new Vector2(moveSpeed, oldVelocity.y + jumpSpeed);
         _rigidbody2D.velocity = newVelocity;
-         
+        if (newVelocity.y > 1) _animator.runtimeAnimatorController = jump;
+        else if (newVelocity.y < -1) _animator.runtimeAnimatorController = fall;
+        else if (newVelocity.x != 0) _animator.runtimeAnimatorController = run;
+        else _animator.runtimeAnimatorController = idle;
+        if (newVelocity.x < 0) gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+        else if (newVelocity.x > 0) gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
     
     private void OnCollisionEnter2D(Collision2D other)
