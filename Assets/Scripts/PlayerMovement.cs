0e2fbeb8367e22fc478e416b7bd6e84f4fac2f38
gtaEPIC <johnnygotrain@gmail.com>
2021-11-11 14:09:09 -0500

    Player can now attack
    Objects that are tagged "Attackable" with the script "Attackable.cs" can be attacked and destroyed when their HP depletes. Each attack deals 1 damage

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Animations;
@@ -7,11 +8,93 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed, jumpPower;
-    public AnimatorController idle, run, jump, fall;
+    public AnimatorController idle, run, jump, fall, attack, attackUp, attackDown, death;
+    public GameObject attackPoint, attackPointUp, attackPointDown;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
-    private int _jumpTimeout;
+    private int[] cooldowns = new int[4]; // 0 = jump, 1 = attack, 2 = attackUp, 3 = attackDown
     private bool _canJump;
+    private bool _attacking;
+    private enum Attacks
+    {
+        none,
+        attackRight,
+        attackLeft,
+        attackUp, 
+        attackDown
+    };
+    
+    public void FinishedAttack()
+    {
+        _attacking = false;
+    }
+
+    private void Attack(GameObject point)
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(point.transform.position, attackPoint.GetComponent<WireMap>().attackRadius);
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Attackable"))
+            {
+                hitCollider.gameObject.GetComponent<Attackable>().OnAttack(1);
+            }
+        }
+    }
+    
+    public void Strike(string direction)
+    {
+        switch (direction)
+        {
+            case "up":
+                Attack(attackPointUp);
+                break;
+            case "down":
+                Attack(attackPointDown);
+                break;
+            case "straight":
+                Attack(attackPoint);
+                break;
+        }
+    }
+    
+
+    private void HandleAnimation(Vector2 velocity, Attacks attackType)
+    {
+        if (_attacking)
+        {
+            return;
+        }
+        if (attackType == Attacks.none)
+        {
+            if (velocity.y > 1) _animator.runtimeAnimatorController = jump;
+            else if (velocity.y < -1) _animator.runtimeAnimatorController = fall;
+            else if (velocity.x != 0) _animator.runtimeAnimatorController = run;
+            else _animator.runtimeAnimatorController = idle;
+            if (velocity.x < 0) gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            else if (velocity.x > 0) gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            _attacking = true;
+            switch (attackType)
+            {
+                case Attacks.attackRight:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    _animator.runtimeAnimatorController = attack;
+                    break;
+                case Attacks.attackLeft:
+                    gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    _animator.runtimeAnimatorController = attack;
+                    break;
+                case Attacks.attackUp:
+                    _animator.runtimeAnimatorController = attackUp;
+                    break;
+                case Attacks.attackDown:
+                    _animator.runtimeAnimatorController = attackDown;
+                    break;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +106,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_jumpTimeout > 0) _jumpTimeout--;
+        // Count down cooldowns
+        for (int i = 0; i < cooldowns.Length; i++)
+        {
+            if (cooldowns[i] > 0) cooldowns[i]--;
+        }
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         float aX = Input.GetAxis("FireHorizontal");
@@ -32,16 +119,22 @@ public class PlayerMovement : MonoBehaviour
         Vector2 oldVelocity = _rigidbody2D.velocity;
         float jumpSpeed = jumpPower;
         float moveSpeed = x * speed;
-        if (!_canJump || _jumpTimeout > 0 || y <= 0) jumpSpeed = 0;
-        else _jumpTimeout = 30;
+        if (!_canJump || cooldowns[0] > 0 || y <= 0) jumpSpeed = 0;
+        else cooldowns[0] = 30;
         Vector2 newVelocity = new Vector2(moveSpeed, oldVelocity.y + jumpSpeed);
         _rigidbody2D.velocity = newVelocity;
-        if (newVelocity.y > 1) _animator.runtimeAnimatorController = jump;
-        else if (newVelocity.y < -1) _animator.runtimeAnimatorController = fall;
-        else if (newVelocity.x != 0) _animator.runtimeAnimatorController = run;
-        else _animator.runtimeAnimatorController = idle;
-        if (newVelocity.x < 0) gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
-        else if (newVelocity.x > 0) gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        Attacks attackType = Attacks.none;
+        if (aX != 0)
+        {
+            if (aX > 0) attackType = Attacks.attackRight;
+            else attackType = Attacks.attackLeft;
+        }
+        else if (aY != 0)
+        {
+            if (aY > 0) attackType = Attacks.attackUp;
+            else attackType = Attacks.attackDown;
+        }
+        HandleAnimation(newVelocity, attackType);
     }
     
     private void OnCollisionEnter2D(Collision2D other)
