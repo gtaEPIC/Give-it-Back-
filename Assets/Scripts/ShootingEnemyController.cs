55bb5033985ad1d2f7124c55f697c60e841ac50a
gtaEPIC <johnnygotrain@gmail.com>
2021-11-12 14:47:08 -0500

    Finished Shooting Enemy
    When in range (can be changed) the shooting enemy will fire at the given rate (per seconds)

--- Assets/Scripts/ShootingEnemyController.cs
+++ Assets/Scripts/ShootingEnemyController.cs
@@ -6,12 +6,23 @@ public class ShootingEnemyController : MonoBehaviour
 {
     public float arrowX, arrowY, fireRate, firePower;
     public GameObject arrow;
+    public GameObject range;
     private float _nextFire;
     
     // Start is called before the first frame update
     void Start()
     {
-        _nextFire = 30 * fireRate;
+        _nextFire = 60 * fireRate;
+    }
+
+    bool InRange()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(range.transform.position, range.GetComponent<WireMap>().attackRadius);
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Player")) return true;
+        }
+        return false;
     }
 
     // Update is called once per frame
@@ -20,12 +31,15 @@ public class ShootingEnemyController : MonoBehaviour
         //Debug.Log(Time.time + "/" + nextFire);
         if (_nextFire == 0)
         {
+            if (!InRange()) return;
             Debug.Log("Firing");
-            _nextFire = fireRate * 30;
+            _nextFire = fireRate * 60;
             Vector2 position = transform.position;
             GameObject arrow = Instantiate(this.arrow, new Vector2(position.x + arrowX, position.y + arrowY),
                 Quaternion.identity);
-            arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(firePower * -1, 0);
+            float fireDirection = firePower;
+            if (transform.rotation.y == 0) fireDirection *= -1;
+            arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(fireDirection, 0);
         }
         else _nextFire--;
     }
